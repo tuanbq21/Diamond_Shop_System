1d3774a7dc1b25aa4266dfd1f7231d1f69bfb5ba
tuanFMV2276 <tuanbqqe170037@fpt.edu.vn>
2024-07-30 19:41:16 +0700

    Update User

--- DSS_SWP/Repositories/UserRepo.cs
+++ DSS_SWP/Repositories/UserRepo.cs
@@ -10,9 +10,17 @@ namespace DSS_SWP.Repositories
 {
     public class UserRepo : BaseDAO<User>
     {
+        private DSS_CustomerContext _Context;
         public UserRepo()
         {
             
+        }
+
+        public User GetAccount(string email, string password)
+        {
+            _context = new DSS_CustomerContext();
+            //&& (a.Role == 2 || a.Role == 1)
+            return _context.Users.FirstOrDefault(a => a.Email == email && a.Password == password);
         }
     }
 }
