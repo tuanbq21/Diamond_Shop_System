bc0e2d484ccd3ea0e1f6ae5d1b6a0d6586e190e7
Ray11224 <manhtdse173612@fpt.edu.vn>
2024-07-31 16:05:07 +0700

    4:04 update orderdetail&payment

--- CustomerView/Pages/Payment.cshtml.cs
+++ CustomerView/Pages/Payment.cshtml.cs
@@ -1,25 +1,43 @@
-using DSS_SWP.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Service.Services;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace CustomerView.Pages
 {
     public class PaymentModel : PageModel
     {
-        private readonly ProductService _productService;
+        public List<ProductViewModel> Products { get; set; }
 
-        public PaymentModel(ProductService productService)
+        public void OnGet()
         {
-            _productService = productService;
+            Products = GetProductsFromCart();
         }
 
-        public List<Product> Products { get; set; }
-
-        public async Task OnGet()
+        public IActionResult OnPost(string email, string name, string address_line1, string address_line2, string address_line3, string phone, string shippingMethod, string paymentMethod)
         {
-            Products = await _productService.GetList();
+            // Xử lý dữ liệu thanh toán
+            // TODO: Lưu đơn hàng vào cơ sở dữ liệu hoặc thực hiện các bước cần thiết khác
+
+            // Chuyển hướng đến trang xác nhận hoặc trang khác
+            return RedirectToPage("OrderConfirmation");
         }
+
+        private List<ProductViewModel> GetProductsFromCart()
+        {
+            // TODO: Lấy danh sách sản phẩm từ giỏ hàng
+            return new List<ProductViewModel>
+            {
+                new ProductViewModel { ProductName = "Sản phẩm 1", ProductCode = "SP001", TotalPrice = 1000000, Image = "/images/product1.jpg" },
+                new ProductViewModel { ProductName = "Sản phẩm 2", ProductCode = "SP002", TotalPrice = 2000000, Image = "/images/product2.jpg" }
+            };
+        }
+    }
+
+    public class ProductViewModel
+    {
+        public string ProductName { get; set; }
+        public string ProductCode { get; set; }
+        public int TotalPrice { get; set; }
+        public string Image { get; set; }
     }
 }
